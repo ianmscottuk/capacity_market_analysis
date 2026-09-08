7f2d54513029f45b9464e8e0b37f75f8a4d41aba
Ian Scott <ianscottuk@gmail.com>
2026-09-08 13:24:58 +0100

    feat: update target capacity and min acceptable prices in baseline scenario

--- src/scenarios.py
+++ src/scenarios.py
@@ -3,17 +3,17 @@ from schemas import CapacityBuyer, CapacityMarketUnit
 
 def baseline_scenario():
     buyer = CapacityBuyer(
-        target_capacity_mw=5000.0,
+        target_capacity=5,
         net_CONE=50,
     )
-
+    # TODO: Organise interms of company (who owns units)
     units = [
         CapacityMarketUnit(
             cmu_id="CMU001",
             name="Drax Power Station",
             parent_company="Drax Group",
             capacity_mw=2600.0,
-            min_acceptable_price=52.0,
+            min_acceptable_price=54.0,
             is_price_taker=False,
         ),
         CapacityMarketUnit(
@@ -30,6 +30,30 @@ def baseline_scenario():
             parent_company="EDF Energy",
             capacity_mw=1300.0,
             min_acceptable_price=39.0,
+            is_price_taker=False,
+        ),
+        CapacityMarketUnit(
+            cmu_id="CMU004",
+            name="Drax Power Station - clone",
+            parent_company="Drax Group",
+            capacity_mw=2600.0,
+            min_acceptable_price=52.0,
+            is_price_taker=False,
+        ),
+        CapacityMarketUnit(
+            cmu_id="CMU005",
+            name="Drax Power Station - clone",
+            parent_company="Drax Group",
+            capacity_mw=100.0,
+            min_acceptable_price=51.0,
+            is_price_taker=False,
+        ),
+        CapacityMarketUnit(
+            cmu_id="CMU006",
+            name="some small unit that exits early",
+            parent_company="EDF Energy",
+            capacity_mw=1300.0,
+            min_acceptable_price=65.0,
             is_price_taker=False,
         ),
     ]

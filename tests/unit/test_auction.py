438f43616475bf9e8ed480016926e6b5ff3968fc
Ian Scott <ianscottuk@gmail.com>
2026-09-13 15:26:27 +0100

    test: add additional test for run_round function

--- tests/unit/test_auction.py
+++ tests/unit/test_auction.py
@@ -13,3 +13,17 @@ def test_run_round_no_units_leave(buyer, units):
     assert remaining_units == units
     assert auction_round.round_number == 1
     assert auction_round.price == buyer.price_cap
+
+
+def test_run_round_all_units_leave(buyer, units):
+    remaining_units, auction_round = run_round(
+        round_number=5,
+        price=10,
+        buyer=buyer,
+        companies=[],
+        active_units=units,
+    )
+
+    assert len(remaining_units) == 0
+    assert auction_round.round_number == 5
+    assert auction_round.price == 10
